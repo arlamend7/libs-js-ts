dddabbdb1e35de5cdbb04a53499030aab277f3bd
dev.arlamend7 <dev.arlamend7@gmail.com>
2021-02-15 23:27:23 -0300

    number symbol interactor extension

--- src/extensions/number.extension.ts
+++ src/extensions/number.extension.ts
@@ -1,3 +1,8 @@
+interface Number {
+  [Symbol.iterator]: Generator<number, number[], unknown>;
+}
+
+//@ts-ignore
 Number.prototype[Symbol.iterator] = function* () {
   for (let i = 0; i <= this; i++) {
     yield i;
